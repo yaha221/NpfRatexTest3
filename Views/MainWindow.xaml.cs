0c993ee58bc5206ebcba542c8d2da5ae928e6221
Yaha <toha.stepanyuck@gmail.com>
2022-08-07 20:04:33 +0300

    Создана папка для представлений и перенесено главное представление

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using NpfRatexTest3.ViewModels;
+using System.Windows;
+
+namespace NpfRatexTest3.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = MainWindowViewModel();
+        }
+    }
+}

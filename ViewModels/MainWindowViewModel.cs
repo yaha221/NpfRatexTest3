12ab7e248f0082159b134a4a853447741c17517b
Yaha <toha.stepanyuck@gmail.com>
2022-08-07 20:06:12 +0300

    Создана модель-представление для главного окна

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Configuration;
+using NpfRatexTest3.Models;
+using NpfRatexTest3.Views;
+
+namespace NpfRatexTest3.ViewModels
+{
+    public class MainWindowViewModel:ViewModel
+    {
+        MainWindowViewModel()
+        {
+
+        }
+    }
+}

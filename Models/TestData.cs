d8843b0ff596e0157ebd18dd67b35962baae577a
Yaha <toha.stepanyuck@gmail.com>
2022-08-07 14:32:02 +0300

    Добавлена модель для отображения данных из БД

---
+++ Models/TestData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NpfRatexTest3.ViewModels;
+
+namespace NpfRatexTest3.Models
+{
+    public class TestData:ViewModel
+    {
+        public int Id { get; set; }
+
+        #region Поле флага
+
+        private bool _flag;
+        /// <summary> Поле флага</summary>
+        public bool Flag
+        {
+            get => _flag;
+            set => Set(ref _flag, value);
+        }
+
+        #endregion
+
+        #region Поле даты
+
+        private string _date;
+        /// <summary> Поле даты </summary>
+        public string Date
+        {
+            get => _date;
+            set => Set(ref _date, value);
+        }
+
+        #endregion
+
+    }
+}

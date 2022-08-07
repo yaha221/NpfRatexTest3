0b00dfceed4d46325986601d1cb3e94c6a950a60
Yaha <toha.stepanyuck@gmail.com>
2022-08-07 14:31:09 +0300

    Добавлен базовый класс ViewModel

---
+++ ViewModels/ViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Threading;
+
+namespace NpfRatexTest3.ViewModels
+{
+    public abstract class ViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            var handlers = PropertyChanged;
+            if(handlers is null)return;
+
+            var invokation_list = handlers.GetInvocationList();
+
+            var arg = new PropertyChangedEventArgs(PropertyName);
+            foreach (var action in invokation_list)
+            {
+                if (action.Target is DispatcherObject disp_object)
+                    disp_object.Dispatcher.Invoke(action, this, arg);
+                else
+                    action.DynamicInvoke(this, arg);
+            }
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
